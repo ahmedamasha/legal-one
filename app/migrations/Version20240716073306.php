1e01c61e169f5ec2c96dd30e6a3fcc531a848418
ahmed amasha <ahmedamasha@ahmeds-MacBook-Pro-2.local>
2024-07-16 10:35:12 +0300

    update the migrations , change db name , user and pass

--- app/migrations/Version20240716073306.php
+++ app/migrations/Version20240716073306.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240715135217 extends AbstractMigration
+final class Version20240716073306 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20240715135217 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE logs (id INT AUTO_INCREMENT NOT NULL, service_name VARCHAR(255) NOT NULL, status_code INT NOT NULL, log_message LONGTEXT NOT NULL, log_timestamp DATETIME NOT NULL, INDEX serviceName_idx (service_name), INDEX statusCode_idx (log_timestamp), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE logs (id INT AUTO_INCREMENT NOT NULL, service_name VARCHAR(255) NOT NULL, status_code INT NOT NULL, log_message LONGTEXT NOT NULL, log_timestamp DATETIME NOT NULL, INDEX serviceName_idx (service_name), INDEX statusCode_idx (status_code), INDEX logTimestamp_idx (log_timestamp), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
